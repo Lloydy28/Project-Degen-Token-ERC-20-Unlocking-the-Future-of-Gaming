355e74809759e9ff9e46398c0e03df15cb8af935
Lloydy28 <128224373+Lloydy28@users.noreply.github.com>
2024-05-29 23:27:13 +0800

    Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Ownable {
+    address public kent;
+    address public newowner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipCancelled(address indexed previousOwner);
+
+    constructor() {
+        kent = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == kent, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(kent, newOwner);
+        kent = newOwner;
+    }
+
+    function cancelTransfer() public onlyOwner {
+        require(newowner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(kent, newowner);
+        kent = newowner;
+    }
+}
